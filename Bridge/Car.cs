255beedd49db3c83c4b15f5bf644f22c0f4e64b3
Kasper Pelle Jensen <kaj007@edu.zealand.dk>
2025-03-05 11:37:00 +0100

    Add project files.

---
+++ Bridge/Car.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    /// <summary>
+    /// Represents a car that has a licenseplate as identifier and a Date for when it is registered in the ticket system.
+    /// </summary>
+    public class Car
+    {
+        private string _licensePlate;
+
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// The cars identifier. Checks whether the license plate only contains numbers and alphabetic characters.
+        /// </summary>
+        public string LicensePlate
+        {
+            get { return _licensePlate; }
+            set
+            {
+                if (!Regex.IsMatch(value, @"^[a-zA-Z0-9]+$"))
+                {
+                    throw new ArgumentException("The license plate can only contain letters and numbers");
+                }
+
+                value = _licensePlate;
+            }
+        }
+
+        public Car(string licensePlate, DateTime date)
+        {
+            LicensePlate = licensePlate;
+            Date = date;
+        }
+
+        /// <summary>
+        /// Determines the ticket price for the car
+        /// </summary>
+        /// <returns>The determined price</returns>
+        public double Price()
+        {
+            return 230;
+        }
+
+        /// <summary>
+        /// Determines the type of the vehicle for the ticket.
+        /// </summary>
+        /// <returns>The determined vehicle type</returns>
+        public string VehicleType()
+        {
+            return "Car";
+        }
+    }
+}
---
+++ BridgeUnitTesting/CarTests.cs
@@ -0,0 +1,60 @@
+﻿using Bridge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeUnitTesting
+{
+    public class CarTests
+    {
+        [Test]
+        public void Car_Price_CorrectPrice_ShouldPass()
+        {
+            //Assign
+            Car testCar = new("test", DateTime.Now);
+            double expectedResult = 230;
+
+            //Act
+            var actualResult = testCar.Price();
+
+
+            //Assert
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void Car_VehicleType_CorrectType_ShouldPass()
+        {
+            //Assign
+            Car testCar = new("test", DateTime.Now);
+            string expectedResult = "Car";
+
+            //Act
+            var actualResult = testCar.VehicleType();
+
+
+            //Assert
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+        }
+
+        [TestCase("@@@@@")]
+        [TestCase("-----")]
+        [TestCase("ABC$%--123")]
+        public void Car_LicensePlate_IllegalCharacters_ShouldThrowException(string licensePlate)
+        {
+            //Assign
+            var expectedResult = new ArgumentException();
+
+
+            //Act
+            TestDelegate actualResult = () => new Car(licensePlate, DateTime.Now);
+
+
+            //Assert
+            Assert.Throws<ArgumentException>(actualResult);
+        }
+    }
+}
